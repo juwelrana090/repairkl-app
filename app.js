003781cb17768e8b759dcd9a8545fe5715cf2987
Dalawer Hossain Juwel <juwelrana090@gmail.com>
2026-06-06 08:48:53 +0600

    update

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 const { createServer } = require("http");
 const { parse } = require("url");
 const next = require("next");
+
 const dev = process.env.NODE_ENV !== "production";
+const port = parseInt(process.env.PORT || "3000", 10);
 
-const port = !dev ? process.env.PORT : 3000;
-
-// Create the Express-Next App
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app
-    .prepare()
-    .then(() => {
-        createServer((req, res) => {
-            const parsedUrl = parse(req.url, true);
-            const { pathname, query } = parsedUrl;
-            handle(req, res, parsedUrl);
-            console.log("pathname", pathname);
-        }).listen(port, (err) => {
-            if (err) throw err;
-            console.log(`> Ready on http://localhost:${port}`);
-        });
-    })
-    .catch((ex) => {
-        console.error(ex.stack);
-        process.exit(1);
+  .prepare()
+  .then(() => {
+    createServer((req, res) => {
+      const parsedUrl = parse(req.url, true);
+      handle(req, res, parsedUrl);
+    }).listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${port}`);
+      console.log(`> Mode: ${dev ? "development" : "production"}`);
     });
+  })
+  .catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
+  });
